326ebb2ea798d09fb182ab7e099f9c52bfe832c3
Marcos Soares <mavmaso@gmail.com>
2018-08-05 10:51:31 -0300

    i18n

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module Jishobr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Brasilia'
+
+    #config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :"pt-BR"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
