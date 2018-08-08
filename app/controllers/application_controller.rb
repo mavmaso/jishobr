1ab46273ebd9dc8253a24579edfc83ae1be81862
Marcos Soares <mavmaso@gmail.com>
2018-08-08 20:10:41 -0300

    [WIP] Adm BAR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :user_parameters, if: :devise_controller?
+
+  protected
+
+  def user_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[name])
+  end
 end
